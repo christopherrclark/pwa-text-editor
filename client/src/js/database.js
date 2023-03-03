928d4d3a30ce752f33987968b66cd30546e27b9e
Fozzy Yeti <christopherrclark@gmail.com>
2023-03-03 09:28:39 -0600

    worked on completing the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,16 +17,16 @@ const initdb = async () =>
 */
 export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  const DB_VAR = await openDB('DB_NAME', 1);
+  const db = await openDB('jate', 1);
 
   // Now create a variable for the transaction
-  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+  const transaction = jateDb.transaction('jate', 'readwrite');
 
   // Now create a variable for the store
-  const STORE_VAR = tx.objectStore('DB_NAME');
+  const store = tx.objectStore('jate');
 
   // Now create a variable named "request" and have it perform the update
-  const VAR_NAME = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
@@ -39,7 +39,15 @@ export const getDb = async () => {
   // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
   
   // LINES 1-3 HERE
+  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+  const db = await openDB('jate', 1);
 
+  // Now create a variable for the transaction
+  const transaction = jateDb.transaction('jate', 'readonly');
+
+  // Now create a variable for the store
+  const store = tx.objectStore('jate');
+  
   // Leave the rest as-is
   const request = store.get(1);
   const result = await request;
